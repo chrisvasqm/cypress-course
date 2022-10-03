7c7498b53abeac37ab1d96a5479753a4baae791e
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-03 16:25:53 -0400

    Add hooks

--- cypress/e2e/mocha-demo.cy.ts
+++ cypress/e2e/mocha-demo.cy.ts
@@ -1,15 +1,29 @@
 describe("Spec 1", () => {
+  before(() => {
+    cy.log("Before hook")
+  })
+
+  after(() => {
+    cy.log("After hook")
+  })
+
+  beforeEach(() => {
+    cy.log("Before each hook")
+  })
+
+  afterEach(() => {
+    cy.log("After each hook")
+  })
 
   it("Test Case 1", () => {
-    cy.log("Hello world");
-  });
+    cy.log("Hello world")
+  })
 
   it("Test Case 2", () => {
-    cy.log("Hello world");
-  });
+    cy.log("Hello world")
+  })
 
   it("Test Case 3", () => {
-    cy.log("Hello world");
-  });
-
-});
+    cy.log("Hello world")
+  })
+})
