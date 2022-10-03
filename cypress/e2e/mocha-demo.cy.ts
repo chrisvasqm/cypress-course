f0ddf98153fb796874a364bf9628a14f5c0d27de
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-03 16:38:38 -0400

    Format code

--- cypress/e2e/mocha-demo.cy.ts
+++ cypress/e2e/mocha-demo.cy.ts
@@ -1,33 +1,33 @@
 describe("Spec 1", () => {
   before(() => {
-    cy.log("Before hook")
-  })
+    cy.log("Before hook");
+  });
 
   after(() => {
-    cy.log("After hook")
-  })
+    cy.log("After hook");
+  });
 
   beforeEach(() => {
-    cy.log("Before each hook")
-  })
+    cy.log("Before each hook");
+  });
 
   afterEach(() => {
-    cy.log("After each hook")
-  })
+    cy.log("After each hook");
+  });
 
   it("Test Case 1", () => {
-    cy.log("Hello world")
-  })
+    cy.log("Hello world");
+  });
 
   it("Test Case 2", () => {
-    cy.log("Hello world")
-  })
+    cy.log("Hello world");
+  });
 
   it.skip("Test Case 3", () => {
-    cy.log("Hello world")
-  })
+    cy.log("Hello world");
+  });
 
   it("Test Case 4", () => {
-    cy.log("Hello world")
-  })
-})
+    cy.log("Hello world");
+  });
+});
