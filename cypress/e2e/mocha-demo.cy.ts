b8b542d44e7c403456412f5ef803151e61bc794e
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-03 16:18:26 -0400

    Add Mocha demo

---
+++ cypress/e2e/mocha-demo.cy.ts
@@ -0,0 +1,15 @@
+describe("Spec 1", () => {
+
+  it("Test Case 1", () => {
+    cy.log("Hello world");
+  });
+
+  it("Test Case 2", () => {
+    cy.log("Hello world");
+  });
+
+  it("Test Case 3", () => {
+    cy.log("Hello world");
+  });
+
+});
