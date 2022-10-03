ceb09aa3998b47d9c23d11b2d989c00b7079675f
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-03 16:56:57 -0400

    Add functions example

--- cypress/e2e/hello-world.cy.ts
+++ cypress/e2e/hello-world.cy.ts
@@ -1,5 +1,11 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.log('Hello world')
-  })
-})
+describe("empty spec", () => {
+  it("passes", () => {
+    cy.log("Hello world");
+  });
+
+  it("functions", () => {
+    const add = (a: number, b: number): number => { return a + b }
+
+    cy.log(add(2, 3).toString());
+  });
+});
