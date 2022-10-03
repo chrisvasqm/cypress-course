3b3f8b98f7470283f8dbb15eb7fdae8fc0aa4190
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-03 16:59:21 -0400

    Add variables example

--- cypress/e2e/hello-world.cy.ts
+++ cypress/e2e/hello-world.cy.ts
@@ -3,8 +3,22 @@ describe("empty spec", () => {
     cy.log("Hello world");
   });
 
+  it("variables", () => {
+    let name: string = "Christian";
+    let age: number = 28;
+    const isAdult = age >= 18;
+    const anything: any = "Anything" + 1;
+
+    cy.log(name);
+    cy.log(age);
+    cy.log(isAdult);
+    cy.log(anything);
+  });
+
   it("functions", () => {
-    const add = (a: number, b: number): number => { return a + b }
+    const add = (a: number, b: number): number => {
+      return a + b;
+    };
 
     cy.log(add(2, 3).toString());
   });
