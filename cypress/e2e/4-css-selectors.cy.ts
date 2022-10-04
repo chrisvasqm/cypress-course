afd1bba1e007587693de7b9efb76e611e05c9554
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-04 11:56:47 -0400

    Add dynamic

---
+++ cypress/e2e/4-css-selectors.cy.ts
@@ -0,0 +1,9 @@
+describe('Locators', () => {
+    beforeEach(() => {
+        cy.visit("/dynamicid")
+    })
+
+    it('Contains example', () => {
+        cy.contains("Button with Dynamic ID").should('exist')
+    });
+})
