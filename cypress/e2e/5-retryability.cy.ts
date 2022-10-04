5a5e6df6baa226ae5c5dc3fea018e53ef825a0c8
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-04 15:40:03 -0400

    Add get element with delay test

--- cypress/e2e/5-retryability.cy.ts
+++ cypress/e2e/5-retryability.cy.ts
@@ -1,5 +1,14 @@
 describe("Retryability", () => {
   it("Visit with delay", () => {
-    cy.visit("/loaddelay", { timeout: 10000});
+    cy.visit("/loaddelay", { timeout: 10000 });
+  });
+
+  it("Get element with delay", () => {
+    cy.visit("/clientdelay");
+    cy.get("#ajaxButton").click();
+    cy.get(".bg-success", { timeout: 20000}).should(
+      "have.text",
+      "Data calculated on the client side."
+    );
   });
 });
