63a84489f4c6efe50ae5ba8cee87316154a100f5
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-05 11:18:00 -0400

    Add env sample

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,5 +6,8 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env: {
+      message: "This is the environment variable",
+    },
   },
 });
