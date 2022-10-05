6e35d5d356d6de27eb4bf79258a090876835440f
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-05 10:24:35 -0400

    Add

---
+++ cypress/e2e/6-ui-scenarios-1.cy.ts
@@ -0,0 +1,13 @@
+describe("UI Scenarios 1", () => {
+  beforeEach(() => {
+    cy.visit("/click");
+  });
+
+  it("click assertion", () => {
+    cy.get("#badButton").click().should("have.class", "btn-success");
+  });
+
+  it("background color assertion", () => {
+    cy.get("#badButton").click().should("have.css", "background-color", "rgb(40, 167, 69)");
+  });
+});
