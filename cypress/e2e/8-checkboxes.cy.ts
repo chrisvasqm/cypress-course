9f77a6f6137bb9eb9eaa3e6181fa88065096c6a9
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-06 10:05:15 -0400

    Add checkbox sample

--- cypress/e2e/8-checkboxes.cy.ts
+++ cypress/e2e/8-checkboxes.cy.ts
@@ -1,7 +1,20 @@
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
+
 describe("Demo QA", () => {
   beforeEach(() => {
-    cy.visit("demoqa.com/checkbox");
+    cy.visit(`${Cypress.env("demoQA")}/checkbox`);
   });
 
-  it("Working with difficult Checkboxes", () => {});
+  it("Working with difficult Checkboxes", () => {
+    cy.get(`input[type="checkbox"]`).click({ force: true });
+    cy.get("#result").should(
+      "have.text",
+      "You have selected :homedesktopnotescommandsdocumentsworkspacereactangularveuofficepublicprivateclassifiedgeneraldownloadswordFileexcelFile"
+    );
+  });
 });
+``;
