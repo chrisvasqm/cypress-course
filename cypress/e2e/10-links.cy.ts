d500b25ba561e12a98d25d013ee2245c71560271
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-10 10:23:03 -0400

    Add link test

---
+++ cypress/e2e/10-links.cy.ts
@@ -0,0 +1,16 @@
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
+
+describe("Demo QA - Links", () => {
+  beforeEach(() => {
+    cy.visit(`${Cypress.env("demoQA")}/links`);
+  });
+
+  it("Validating links without switching tabs", () => {
+    cy.get("#simpleLink").should("have.attr", "href", "https://demoqa.com");
+    cy.get("#simpleLink").should("have.attr", "target", "_blank");
+  });
+});
