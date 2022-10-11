5a5ba02069f13d548a9d0fb3ac8347623f8c3c1d
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-11 10:46:21 -0400

    Add broken image tests

---
+++ cypress/e2e/11-broken-images-and-links.cy.ts
@@ -0,0 +1,31 @@
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // returning false here prevents Cypress from
+  // failing the test
+  return false;
+});
+
+let image: HTMLImageElement;
+
+describe("Demo QA - Broken Images and Links", () => {
+  beforeEach(() => {
+    cy.visit(`${Cypress.env("demoQA")}/broken`);
+  });
+
+  it("Valid Image", () => {
+    cy.get(`div > img[src="/images/Toolsqa.jpg"]`)
+      .should("be.visible")
+      .and(($image) => {
+        image = $image[0] as HTMLImageElement;
+        expect(image.naturalWidth).to.be.greaterThan(0);
+      });
+  });
+
+  it("Broken Images", () => {
+    cy.get(`div > img[src="/images/Toolsqa_1.jpg"]`)
+      .should("be.visible")
+      .and(($image) => {
+        image = $image[0] as HTMLImageElement;
+        expect(image.naturalWidth).to.be.greaterThan(0);
+      });
+  });
+});
