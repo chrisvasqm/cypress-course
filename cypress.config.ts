3f8b7db61be64bf69856c1ac0d5781081f817ff5
chrisvasqm <christianvasquezmelo@gmail.com>
2022-10-14 11:17:08 -0400

    Add HTML report

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,12 +6,21 @@ export default defineConfig({
     baseUrl: "http://uitestingplayground.com",
     setupNodeEvents(on, config) {
       on("task", { isFileExist, findFiles });
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
     env: {
       message: "This is the environment variable",
       uitestplayground: "http://uitestingplayground.com",
       demoQA: "https://demoqa.com",
       internetapp: "https://the-internet.herokuapp.com",
+    },
+    reporter: "cypress-mochawesome-reporter",
+    reporterOptions: {
+      charts: true,
+      reportPageTitle: "Udemy Cypress Course",
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false,
     },
   },
 });
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import "./commands";
 import "cypress-real-events/support";
+import "cypress-mochawesome-reporter/register";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
